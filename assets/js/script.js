93bb39efadedc2ce4e8a0d6e5ce06cfaf69d6aa1
denisklopotan <denisklopotan@gmail.com>
2021-08-13 01:25:15 +0000

    Add Javascript and minor changes in HTML

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+      document.getElementById('testid').addEventListener('submit', function(event){
+      event.preventDefault();
+      
+      var vatascore = 0;
+      var pittascore = 0;
+      var kaphascore = 0;
+      
+      var choices = document.getElementsByTagName('input');
+      for (i=0; i<choices.length; i++) {
+        if (choices[i].checked) {
+          if (choices[i].value == 'vata') {
+              vatascore = vatascore + 1;
+          }
+          if (choices[i].value == 'pitta') {
+              pittascore = pittascore + 1;
+          }
+          if (choices[i].value == 'kapha') {
+              kaphascore = kaphascore + 1;
+          }
+        }
+      }
+    let result = document.getElementById('results');
+    result.innerHTML = "VATA: " + vatascore + ", PITTA: " + pittascore + ", KAPHA: " + kaphascore;
+    })
+
+
